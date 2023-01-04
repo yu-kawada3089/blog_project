17d57402b69d8a4ba0f822fb9586fd5852070905
kawa_yu <kawady10@gmail.com>
2023-01-04 21:32:40 +0900

    画面表示の不具合の修正

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -7,8 +7,8 @@ import Link from "next/link";
 export default function Portfolio() {
   return (
     <Layout>
-      <Link href="/test_html_2023" passHref>
-        あああ
+      <Link href="/test_html_2023.html" passHref>
+        ケーススタディを載せる予定
       </Link>
     </Layout>
   );
